f9ba5960c437ded62d5032c6a2b25b17ff931c52
Tri Nguyen <tringuye@unc.edu>
2025-11-11 00:05:35 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Tri Nguyen on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
